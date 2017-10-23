88eb2783ffe182be37fc04cc88800d9cd9b2372c
Marino Linaje <marino.linaje@gmail.com>
2017-10-24 00:06:08 +0200

    Arduino Serial Tunnel
    Useful to use with e.g., AT command-based modules (SIM800, SIM808, bluetooth,...)

---
+++ SerialTunnel.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(3, 2); // RX, TX
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+  // set the data rate for the SoftwareSerial port
+  mySerial.begin(115200);
+}
+
+void loop() { // run over and over
+  if (mySerial.available()) {
+    Serial.write(mySerial.read());
+  }
+  if (Serial.available()) {
+    mySerial.write(Serial.read());
+  }
+}
