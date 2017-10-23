ec687d169b147c6b3e7d592aa7f6d2d782af5b96
Marino Linaje <marino.linaje@gmail.com>
2017-10-23 11:27:47 +0200

    Módulo detección probado
     Probado en wemos (esp8266) arduino mega y arduino uno

---
+++ rcwl-0516.ino
@@ -0,0 +1,39 @@
+#ifdef ESP8266
+  #define Presencia_GPIO D2
+#else
+  #define Presencia_GPIO 4
+#endif
+
+#define LED_GPIO LED_BUILTIN
+
+bool val = 0;
+bool old_val = 0;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode (Presencia_GPIO, INPUT);
+  pinMode (LED_GPIO, OUTPUT);
+  old_val = digitalRead(Presencia_GPIO);
+  Serial.flush();
+  Serial.println("");
+}
+
+void loop() {
+  val = digitalRead(Presencia_GPIO);
+
+  if (old_val != val){
+    if (val)
+      Serial.println("DETECTADO CUERPO EN MOVIMIENTO");
+    else
+      Serial.println("TODO TRANQUILO");
+
+#ifdef ESP8266
+      digitalWrite(LED_GPIO, !val); //lógica inversa del ESP8266
+#else
+      digitalWrite(LED_GPIO, val);
+#endif
+    old_val = val;
+  }
+  //delay(1000);
+  //yield();
+}
